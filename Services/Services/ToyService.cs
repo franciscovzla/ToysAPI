05f22d954d0a7feaa71e398f5e3617a163d5c54e
Francisco Valenzuela <francisco.valenzuela@unosquare.com>
2022-06-14 12:10:06 -0700

    Removed old code

--- Services/Services/ToyService.cs
+++ Services/Services/ToyService.cs
@@ -1,13 +1,6 @@
 ﻿using Services.Contracts;
 using Services.DTO;
 using Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using System.Web.Mvc;
 using Data;
 using Microsoft.EntityFrameworkCore;
 using AutoMapper;
@@ -30,44 +23,12 @@ namespace Services.Services
             return _context.Toys.ToList().Select(x => _mapper.Map<Toys, ToysViewModel>(x)).ToList();
         }
 
-            //    List <ToysViewModel> toys = _context.Toys.ToListAsync();
-            //    return _mapper.Map<ToysViewModel>(toys);
-            //}
-            //_context.Toys
-            //.Select(x => new ToysViewModel
-            //{
-            //    Id = x.Id,
-            //    Name = x.Name,
-            //    Description = x.Description,
-            //    AgeRestriction = x.AgeRestriction,
-            //    CompanyId = x.CompanyId,
-            //    Price = x.Price
-            //})
-            //.ToListAsync();
-
-
-
         public async Task<ToysViewModel?> GetToyById(int? id)
         {
             var Toy = await _context.Toys.FirstOrDefaultAsync(x => x.Id == id);
             return _mapper.Map<ToysViewModel>(Toy);
 
         }
-
-
-        //_context.Toys
-        //  .Select(x => new ToysViewModel
-        //  {
-        //      Id = id,
-        //      Name = x.Name,
-        //      Description = x.Description,
-        //      AgeRestriction = x.AgeRestriction,
-        //      CompanyId = x.CompanyId,
-        //      Price = x.Price
-        //  })
-        //  .FirstOrDefaultAsync(x => x.Id == id);
-
-
 
         public async Task AddToy(ToysDTO toy)
         {
@@ -82,25 +43,6 @@ namespace Services.Services
                 throw new Exception($"Toy could not been added {ex}");
             }
 
-            //    try
-            //    {
-            //        var newToy = new Toys
-
-            //        {
-            //            Name = toy.Name,
-            //            Description = toy.Description,
-            //            AgeRestriction = toy.AgeRestriction,
-            //            CompanyId = toy.CompanyId,
-            //            Price = toy.Price
-            //        };
-            //        _context.Toys.Add(newToy);
-            //        await _context.SaveChangesAsync();
-            //    }
-            //    catch (Exception)
-            //    {
-
-            //        throw new Exception("Toy could not been added");
-            //    }
         }
 
         public async Task<bool> UpdateToy(ToysDTO request)
@@ -120,31 +62,7 @@ namespace Services.Services
             {
                 throw new Exception($"Toy could not been modified {ex}");
             }
-            //try
-            //{
-            //    var Toy = await _context.Toys.FirstOrDefaultAsync(x => x.Id == request.Id);
-            //    if (Toy is null)
-            //    {
-            //        return false;
-            //    }
 
-            //    //TODO: Try to use Automapper instead
-            //    Toy.Name = request.Name;
-            //    Toy.Description = request.Description;
-            //    Toy.AgeRestriction = request.AgeRestriction;
-            //    Toy.CompanyId = request.CompanyId;
-            //    Toy.Price = request.Price;
-
-
-            //    await _context.SaveChangesAsync();
-
-            //    return true;
-            //}
-            //catch (Exception)
-            //{
-
-            //    throw new Exception("Toy could not been modified");
-            //}
         }
 
         public async Task<bool> DeleteToy(int? id)
@@ -157,8 +75,6 @@ namespace Services.Services
                 {
                     return false;
                 }
-
-
                 _context.Remove(Toy);
                 await _context.SaveChangesAsync();
                 return true;
