b1940a8ccce148c992b50c38d39b4fdddc2ccdf4
Francisco Valenzuela <francisco.valenzuela@unosquare.com>
2022-06-14 12:41:09 -0700

    Removed useless code

--- ToysAPI/Program.cs
+++ ToysAPI/Program.cs
@@ -15,14 +15,14 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), options=> { options.MigrationsAssembly("Data"); });
 });
 
-//builder.Services.AddScoped<DbContext, DataContext>();
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddServ();
-//builder.Services.AddScoped<IToyService, ToyService>();
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
